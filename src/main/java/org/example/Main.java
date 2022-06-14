0a19f130c0e3a2d835581a8d16742b928c96d966
Jonas Eira <jonas.eira@gmail.com>
2022-06-14 09:35:44 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        NameRepository nr = new NameRepository();
+    }
+}
---
+++ src/main/java/org/example/NameRepository.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class NameRepository {
+    public NameRepository(){
+
+    }
+
+    public static void getName()
+
+    public static void setNames(String[] names){
+
+    }
+
+    
+
+}
