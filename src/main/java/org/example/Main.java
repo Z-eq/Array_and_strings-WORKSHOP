7793503629f60a789e7446015d40b559f9c0bc0c
Jonas Eira <jonas.eira@gmail.com>
2022-06-14 11:06:01 +0200

    Added all code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,27 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         NameRepository nr = new NameRepository();
+        String[] tmp = new String[]{
+                "Zeq Alidemaj",
+                "Jonas Eira"
+        };
+        NameRepository nr1 = new NameRepository(tmp);
+        NameRepository.add("Simon Elbrink");
+        NameRepository.add("Zeq Alidemaj1");
+        NameRepository.add("Zeq Alidemaj2");
+        NameRepository.add("Zeq Alidemaj3");
+        NameRepository.add("Zeq Alidemaj4");
+        NameRepository.add("Zeq Alidemaj5");
+        for(String name : NameRepository.findAll()){
+            System.out.println("Name:" + name);
+        }
+        System.out.println("Names size = " + NameRepository.getSize());
+
+        String[] firstNames = NameRepository.findByFirstName("Zeq");
+
+        for(String name : firstNames){
+            System.out.println("Name:" + name);
+        }
+
     }
 }
--- src/main/java/org/example/NameRepository.java
+++ src/main/java/org/example/NameRepository.java
@@ -1,16 +1,111 @@
 package org.example;
 
 public class NameRepository {
-    public NameRepository(){
+    //Our internal array
+    private static String[] names;
 
+
+    public NameRepository() {
+        names = new String[1];
     }
 
-    public static void getName()
-
-    public static void setNames(String[] names){
-
+    public NameRepository(String[] tmp) {
+        names = new String[1];
+        setNames(tmp);
     }
 
-    
+    /*
+     * Returns the length of names.
+     */
+    public static int getSize() {
+        return names.length;
+    }
 
+    /*
+     *  Deep copy of string array to the object
+     */
+    public static void setNames(String[] names2) {
+        if (names.length < names2.length) {
+            // expand the internal array
+            names = new String[names2.length];
+        }
+        for (int n = 0; n < names2.length; n++) {
+            names[n] = names2[n];
+        }
+    }
+
+    /*
+     * Clears all the names in the array
+     */
+    public static void clear() {
+        names = new String[1];
+    }
+
+    /*
+     * Returns all the names in the array
+     */
+    public static String[] findAll() {
+        return names;
+    }
+
+    /*
+     *   Finds a name in the database
+       @return null if no name is found
+
+     */
+    public static String find(final String fullName) {
+        for (int n = 0; n < names.length; n++) {
+            if (names[n] == fullName) {
+                return names[n];
+            }
+        }
+        return null;
+    }
+
+    /*
+     *  This method adds another name to the database
+     *  @return true if added
+     */
+    public static boolean add(final String fullName) {
+        if (find(fullName) == null) {
+            //adding the name here
+            String[] names2 = new String[names.length + 1];
+            for (int n = 0; n < names.length; n++) {
+                names2[n] = names[n];
+            }
+            names2[names2.length - 1] = fullName;
+            names = names2;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /*
+     *  This method finds all names by first name
+     */
+    public static String[] findByFirstName(final String firstName) {
+        int counter = 0;
+        for (String fullName : names) {
+            if (fullName.startsWith(firstName)) {
+                counter++;
+            }
+        }
+        String[] tmp = new String[counter];
+        counter = 0;
+        for (String fullName : names) {
+            if (fullName.startsWith(firstName)) {
+                tmp[counter++] = fullName;
+            }
+        }
+        return tmp;
+    }
+
+    public static String[] findByLastName(final String lastName) {
+
+        int counter = 0;
+        for ()
+    }
 }
+
+    "Zeq "| Startswith"Alidemaj"
